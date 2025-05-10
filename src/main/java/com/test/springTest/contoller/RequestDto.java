1759340958a8ee0c4210fe7219c3042f049b982a
ngrjpatel <ngrjpatel@gmail.com>
2025-05-10 13:07:55 +0530

    sysout added

--- src/main/java/com/test/springTest/contoller/RequestDto.java
+++ src/main/java/com/test/springTest/contoller/RequestDto.java
@@ -1,9 +1,15 @@
 package com.test.springTest.contoller;
 
 import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class RequestDto {
     private String id;
     private String name;
